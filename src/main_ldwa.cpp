697468279899312684bd542c13decc32d4e010c5
Chinemelu Ezeh <chinemeluezeh@gmx.com>
2016-10-23 13:27:43 +0100

    Linear Blending Working

--- src/main_ldwa.cpp
+++ src/main_ldwa.cpp
@@ -7,6 +7,6 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "linear_dwa");
 	ros::NodeHandle n;
 
-	LinearDWA linear_dwa = LinearDWA(n);
+	LinearDWA linear_dwa = LinearDWA("linear_dwa",n);
 	linear_dwa.run();
 }
